0766217dad1b55343aef3236f3f3de183e778968
devofficer <nikolaytoplev@gmail.com>
2022-09-07 10:41:57 +1000

    fix: Wasm warning

--- src/utils/constants.template.ts
+++ src/utils/constants.template.ts
@@ -6,8 +6,8 @@ export const WRAPPED_GAS_TOKEN_ADDRESS = "{{wrapped_gas_token_address}}";
 export const AX_ADDRESS = "{{ax_token_address}}";
 export const FACTORY_ADDRESS = "{{ax_factory_address}}";
 
-export const ZERO_BI = BigInt.fromI32(0);
-export const ONE_BI = BigInt.fromI32(1);
-export const ZERO_BD = BigDecimal.fromString("0");
-export const ONE_BD = BigDecimal.fromString("1");
-export const BI_18 = BigInt.fromI32(18);
+export let ZERO_BI = BigInt.fromI32(0);
+export let ONE_BI = BigInt.fromI32(1);
+export let ZERO_BD = BigDecimal.fromString("0");
+export let ONE_BD = BigDecimal.fromString("1");
+export let BI_18 = BigInt.fromI32(18);
